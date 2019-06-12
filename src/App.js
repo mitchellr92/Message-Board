3beb7b53aa0cc932dabdc366794ed5f9234e591e
Mitchell Robles <mitchellr92@live.com>
2019-06-12 09:59:01 -0700

    connected front end to back end

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ import Authenticate from "./Authentication/Authentication";
 import Photos from "./Components/Photos/Photos";
 import PostContainer from "./Components/Posts/PostContainer";
 import NewPost from "./Components/Posts/NewPost";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  getPosts = event => {
+    axios
+      .get(`http://localhost:1111/api/home`)
+      .then(response => {
+        this.setState({
+          posts: response.data
+        });
+        console.log(this.state.posts)
+      })
+      .catch(err => console.log("Failure to load posts"));
+  };
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
   logoutHandler = event => {
     localStorage.clear();
     window.location.reload();
